8e87a660d11ec19d4e5cc2b0635fc9f5f1cd1861
Alex-Sokol-creator <gamerodessa2019@gmail.com>
2026-04-30 22:15:38 +0300

    Dz 1.2 was changed

--- Dz 1.2/Dz 1.2.cpp
+++ Dz 1.2/Dz 1.2.cpp
@@ -4,6 +4,14 @@
 
 int main()
 {
-    std::cout << "First Task";
+    setlocale(LC_ALL, "Russian");
+
+    std::cout << "First Task" << '\n';
+    std::cout << "Введите время в секундах для их конвертации: ";
+    long seconds_time;
+    std::cin >> seconds_time;
+    double minuts_time = seconds_time / 60.0;
+    std::cout << '\n' << "Время в минутах: " << minuts_time;
+
 }
 
