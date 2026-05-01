39545dfbec5cb705b6a5830e0b3f4c5aad25df7f
Alex-Sokol-creator <gamerodessa2019@gmail.com>
2026-05-01 23:30:55 +0300

    Dz 1.2 was updated

--- Dz 1.2/Dz 1.2.cpp
+++ Dz 1.2/Dz 1.2.cpp
@@ -11,7 +11,16 @@ int main()
     long seconds_time;
     std::cin >> seconds_time;
     double minuts_time = seconds_time / 60.0;
-    std::cout << '\n' << "Время в минутах: " << minuts_time;
+    std::cout << '\n' << "В минутах это: " << minuts_time << "минут(ы)";
+    double hours_time = seconds_time / 3600.0;
+    std::cout << '\n' << "В часах это: " << hours_time << "часа(ов)";
+    std::cout << '\n' << "В секундах это: " << seconds_time << "секунд(ы)";
 
+    std::cout << '\n' << "Second Task" << '\n';
+    std::cout << "Введите денежную дробь для ее конвертации в гривны и копейки: ";
+    float money;
+    std::cin >> money;
+    int money_general = money;
+    std::cout << '\n' << money_general;
 }
 
