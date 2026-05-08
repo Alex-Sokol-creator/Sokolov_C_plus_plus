5c942b990094917108666998b9e72d4a76523c07
Alex-Sokol-creator <gamerodessa2019@gmail.com>
2026-05-08 23:18:33 +0300

    Some objects were removed and dz 2 was started

---
+++ Homeworks/Dz 2 Условные конструкции/Dz 2 Условные конструкции.cpp
@@ -0,0 +1,148 @@
+﻿
+
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	srand(time(NULL));
+
+	/* {
+		cout << "\nFirst Task\n";
+		int number;
+		int maxnum;
+
+		cout << "Введите 1 число: ";
+		cin >> number;
+		maxnum = number;
+		for (int i = 2; i < 8; i++) {
+			cout << "Введите " << i << " число: ";
+			cin >> number;
+			if (number > maxnum) {
+				maxnum = number;
+			}
+		}
+		cout << "Максимальное число из введеных вами 7 это: " << maxnum;
+	}*/
+
+	/* {
+		cout << "\nSecond Task\n";
+		int maxfuel = 300;
+		int spending;
+		double weight;
+		double ab;
+		double bc;
+		double ac;
+		char possibility = 1;
+		cout << "Введите вес груза на самолете в кг: ";
+		cin >> weight;
+		if (weight <= 500) {
+			spending = 1;
+		}
+		else if (weight <= 1000 && weight > 500) {
+			spending = 4;
+		}
+		else if (weight <= 1500 && weight > 1000) {
+			spending = 7;
+		}
+		else if (weight <= 2000 && weight > 1500) {
+			spending = 9;
+		}
+		else {
+			cout << "Самолет не может взлететь с весом более 2000 кг";
+			possibility = 0;
+		}
+		if (possibility != 0) {
+			cout << "Введите расстояние от пункта А до пункта В: ";
+			cin >> ab;
+			cout << "Введите расстояние от пункта B до пункта C: ";
+			cin >> bc;
+			double fuelab = ab * spending;
+			double fuelbc = bc * spending;
+			if (fuelab > maxfuel || fuelbc > maxfuel) {
+				cout << "К сожалению, самолет не может преодолеть такое расстояние с таким грузом\n";
+			}
+			else {
+				double needfuel;
+				double charging = 0;
+				double fuelablast = maxfuel - fuelab;
+				if (fuelbc < fuelablast) {
+					needfuel = fuelab + fuelbc;
+				}
+				else {
+					charging = fuelbc - fuelablast;
+					needfuel = fuelab + charging;
+				}
+				cout << "Так как груз весит " << weight << " кг, то расход топлива будет " << spending << "л/км,\nи для того, чтобы пролететь " << ab + bc
+					<< "км, вам минимально потребуется " << needfuel << "л топлива. \nДозаправлять в пункте В надо будет " << charging << "л топлива.";
+			}
+		}
+	}*/
+
+	{
+		cout << "\nThird Task\n";
+		int salary = 200;
+		int bonus = 200;
+		double saldop1;
+		double saldop2;
+		double saldop3;
+		double allsal1, allsal2, allsal3;
+		int sells;
+		int bestmanager;
+		cout << "Введите объемы продаж в $ 1 менеджера: ";
+		cin >> sells;
+		if (sells < 500) {
+			saldop1 = sells * 0.03;
+		}
+		else if (sells < 1000 && sells >= 500) {
+			saldop1 = sells * 0.05;
+		}
+		else if (sells >= 1000) {
+			saldop1 = sells * 0.08;
+		}
+		cout << "Введите объемы продаж в $ 2 менеджера: ";
+		cin >> sells;
+		if (sells < 500) {
+			saldop2 = sells * 0.03;
+		}
+		else if (sells < 1000 && sells >= 500) {
+			saldop2 = sells * 0.05;
+		}
+		else if (sells >= 1000) {
+			saldop2 = sells * 0.08;
+		}
+		cout << "Введите объемы продаж в $ 3 менеджера: ";
+		cin >> sells;
+		if (sells < 500) {
+			saldop3 = sells * 0.03;
+		}
+		else if (sells < 1000 && sells >= 500) {
+			saldop3 = sells * 0.05;
+		}
+		else if (sells >= 1000) {
+			saldop3 = sells * 0.08;
+		}
+		allsal1 = salary + saldop1;
+		allsal2 = salary + saldop2;
+		allsal3 = salary + saldop3;
+		if (allsal1 > allsal2 && allsal1 > allsal3) {
+			allsal1 += bonus;
+			bestmanager = 1;
+		}
+		else if (allsal1 < allsal2 && allsal2 > allsal3) {
+			allsal2 += bonus;
+			bestmanager = 2;
+		}
+		else if (allsal2 < allsal3 && allsal3 > allsal1) {
+			allsal3 += bonus;
+			bestmanager = 3;
+		}
+		cout << "Наилучший менеджер это " << bestmanager << " менеджер, у него зарплата " << allsal3 << "\n"
+
+	}
+}
+
