8a2eee0506a90f0b823930a977626eaf12dff2a9
Alex-Sokol-creator <gamerodessa2019@gmail.com>
2026-05-16 13:47:20 +0300

    Lesson 6 was created

---
+++ Classworks/Lesson 6 Алгоритмы/Lesson 6 Алгоритмы.cpp
@@ -0,0 +1,83 @@
+﻿
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    srand(time(NULL));
+
+    const int size = 7;
+    int arr[size];
+
+    int min = -10;
+    int max = 10;
+
+    for (int i = 0; i < size; i++) {
+        arr[i] = rand() % (max - min + 1) + min;
+    }
+    
+    cout << "Unsorted array: ";
+    for (int num : arr) {
+        cout << num << ' ';
+    }
+    cout << '\n';
+
+    //Select sort
+    /*
+    // Select sort
+    int minIndex;
+    for (int i = 0; i < size; i++) {
+        minIndex = i;
+
+        for (int j = i; j < size; j++) {
+            if (arr[minIndex] > arr[j]) {
+                minIndex = j;
+            }
+        }
+
+        if (minIndex != i) {
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    */
+
+    //Bubble sort
+    /*
+    //Bubble sort
+    for (int i = 0; i < size; i++) {
+        for (int j = size - 1; j > i; j--) {
+            if (arr[j - 1] > arr[j]) {
+                int temp = arr[j - 1];
+                arr[j - 1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    */
+
+    //Insertion sort
+    /*
+    for (int i = 1; i < size; i++) {
+        int temp = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > temp) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = temp;
+    }*/
+
+    cout << "Unsorted array: ";
+    for (int num : arr) {
+        cout << num << ' ';
+    }
+    cout << '\n';
+}
+
