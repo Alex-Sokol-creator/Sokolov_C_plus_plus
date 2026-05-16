99987f8515bad60a21f2c0d7da4db300e6a7f7dc
Alex-Sokol-creator <gamerodessa2019@gmail.com>
2026-05-16 15:26:58 +0300

    Dz 5 was created and Practice 6

---
+++ Homeworks/Dz 5 Множественные массивы/Dz 5 Множественные массивы.cpp
@@ -0,0 +1,45 @@
+﻿
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    {
+        cout << "\nFirst Task\n";
+        const int size = 3;
+        int masiv[size][size];
+        int num1, num2, num3, numindex = 0;
+        cout << "Введите 1 число для заполнения двухмерного массива: ";
+        cin >> num1;
+        /*
+        cout << "Введите 2 число для заполнения двухмерного массива: ";
+        cin >> num2;
+        cout << "Введите 3 число для заполнения двухмерного массива: ";
+        cin >> num3;
+        int numbers[3] = { num1,num2,num3 };
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (j == 0) {
+                    masiv[i][j] = numbers[numindex];
+                }
+                else {
+                    masiv[i][j] = masiv[i][j - 1] * 2;
+                }
+            }
+            numindex += 1;
+        }*/
+        cout << "Вот ваш итоговый массив: \n";
+        for (int i = 0; i < size; i++) {
+            for (int num : masiv[i]) {
+                cout << num << ' ';
+            }
+            cout << '\n';
+        }
+    }
+}
+
