ee0320914143f0cc1f3aded36d7e6f19d4e335f3
Alex-Sokol-creator <gamerodessa2019@gmail.com>
2026-05-23 15:50:19 +0300

    Lesson 8 was ended and Dz 6 was updated

---
+++ Classworks/Lesson 8 Inline-функции/Lesson 8 Inline-функции.cpp
@@ -0,0 +1,201 @@
+﻿
+/*#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+#define SQR(x) ((x) * (x))
+
+inline double add(double a, double b) {
+    return a + b;
+}
+
+int sum(int a, int b) {
+    return a + b;
+}
+
+float sum(float a, float b) {
+    return a + b;
+}
+float sum(float a, float b, float c) {
+    return a + b + c;
+}
+
+int findMax(int, int);
+int findMax(int[], int);
+double findMax(double,double,double);
+double findMax(double[], int);
+
+template<typename T>
+T findMax(T arr[], int size);
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int integers[] = { 9, -5,7,5,6,8 };
+    double doubles[] = { 9.8, 6.7,5.6,7.3 };
+    float floates[] = { 9.5f,6.7f,43.6f };
+
+    //cout << SQR(10) << '\n';
+    //cout << 10 * 10 << '\n';
+
+    //for (int i = 0; i < 20; i++) {
+    //    cout << add((double)i, (double)(i * 150)) + add(10,12)<< ' ';
+    //    //cout << (double)i + (double)(i * 150) << ' '; 
+    //}
+    //cout << '\n';
+
+    //cout << sum(10, 12) << '\n';
+    //cout << sum(12.4f, 23.56f) << '\n';
+    //cout << sum(12.4f, 23.56f,8.76f) << '\n';
+
+    findMax(integers, 5);
+    findMax(doubles, 4);
+    findMax(floates, 3);
+}
+
+int findMax(int num1, int num2) {
+    return (num1 > num2) ? num1 : num2;
+}
+
+//int findMax(int arr[], int size) {
+//    int max = arr[0];
+//    for (int i = 1; i < size; i++) {
+//        if (max < arr[i]) {
+//            max = arr[i];
+//        }
+//    }
+//    return max;
+//}
+
+double findMax(double num1, double num2, double num3) {
+    return (num1 > num2) ?
+        ((num1 > num3) ? num1 : num3) :
+        ((num2 > num3) ? num2 : num3);
+}
+
+//double findMax(double arr[], int size) {
+//    int max = arr[0];
+//    for (int i = 1; i < size; i++) {
+//        if (max < arr[i]) {
+//            max = arr[i];
+//        }
+//    }
+//    return max;
+//}
+
+template<typename T>
+T findMax(T arr[], int size) {
+    T max = arr[0];
+    for (int i = 1; i < size; i++) {
+        if (max < arr[i]) {
+            max = arr[i];
+        }
+    }
+    return max;
+}*/
+
+
+
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+#define SQR(x) ((x) * (x))
+
+inline double add(double a, double b) {
+    return a + b;
+}
+
+int sum(int a, int b) {
+    return a + b;
+}
+
+float sum(float a, float b) {
+    return a + b;
+}
+float sum(float a, float b, float c) {
+    return a + b + c;
+}
+
+// Удалены прототипы для int[] и double[], так как их заменяет шаблон
+int findMax(int, int);
+double findMax(double, double, double);
+
+// Реализация шаблона должна быть видна компилятору до вызова
+template<typename T>
+T findMax(T arr[], int size) {
+    T max = arr[0]; // Исправлено: тип T вместо int
+    for (int i = 1; i < size; i++) {
+        if (max < arr[i]) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+void recursionFunction(int counter) {
+    cout << counter++ << '\n';
+    recursionFunction(counter);
+}
+
+int factorialIterational(int num) {
+    int result = 1;
+    for (int i = 1; i <= num; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+int factorialRecursion(int num) {
+    if (num == 1) {
+        return 1;
+    }
+    else {
+        return num * factorialRecursion(num - 1);
+    }
+}
+
+int fibonacci(int number) {
+    if (number <= 1) {
+        return number;
+    }
+    else {
+        return fibonacci(number - 1) + fibonacci(number - 2);
+    }
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int integers[] = { 9, -5, 7, 5, 6, 8 };
+    double doubles[] = { 9.8, 6.7, 5.6, 7.3 };
+    float floates[] = { 9.5f, 6.7f, 43.6f };
+
+    // Теперь эти вызовы используют один универсальный шаблон
+    cout << "Int max: " << findMax(integers, 6) << endl;
+    cout << "Double max: " << findMax(doubles, 4) << endl;
+    cout << "Float max: " << findMax(floates, 3) << endl;
+
+    //recursionFunction(0);
+
+    cout << factorialIterational(5) << '\n';
+    cout << factorialRecursion(5) << '\n';
+
+    cout << fibonacci(5) << '\n';
+}
+
+int findMax(int num1, int num2) {
+    return (num1 > num2) ? num1 : num2;
+}
+
+double findMax(double num1, double num2, double num3) {
+    return (num1 > num2) ?
+        ((num1 > num3) ? num1 : num3) :
+        ((num2 > num3) ? num2 : num3);
+}
--- Homeworks/Dz 6 Алгоритмы сортировки/Dz 6 Алгоритмы сортировки.cpp
+++ Homeworks/Dz 6 Алгоритмы сортировки/Dz 6 Алгоритмы сортировки.cpp
@@ -6,86 +6,144 @@ using namespace std;
 
 int main()
 {
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	srand(time(NULL));
 
-    /*
-    for (int i = 1; i < size; i++) {
-        int temp = arr[i];
-        int j = i - 1;
+	/*
+	for (int i = 1; i < size; i++) {
+		int temp = arr[i];
+		int j = i - 1;
 
-        while (j >= 0 && arr[j] > temp) {
-            arr[j + 1] = arr[j];
-            j--;
-        }
+		while (j >= 0 && arr[j] > temp) {
+			arr[j + 1] = arr[j];
+			j--;
+		}
 
-        arr[j + 1] = temp;
-    }*/
+		arr[j + 1] = temp;
+	}*/
 
-    {
-        cout << "\nFirst Task\n";
-        const int size = 5;
-        long long mobilephones[size] = { 3801234567,3705869854,3698745126,1522356587,1321254787 };
-        long long homephones[size] = { 9443654789,98745621,457456221,8765874512,9824512360 };
-        int choice;
-        cout << "Справочник\n";
-        while (true) {
-            choice = 0;
-            while (choice != 1 && choice != 2 && choice != 3 && choice != 4) {
-                cout << "Что вы хотите сделать? (1 - отсортировать по мобильным номерам,2 - отсортировать по домашним номерам,\n3 - вывести список пользователей, 4 - выход): ";
-                cin >> choice;
-            }
-            switch (choice) {
-            case 1: {
-                for (int i = 0; i < size; i++) {
-                    for (int j = size - 1; j > i; j--) {
-                        if (mobilephones[j - 1] > mobilephones[j]) {
-                            long long temp = mobilephones[j - 1];
-                            long long temp2 = homephones[j - 1];
-                            mobilephones[j - 1] = mobilephones[j];
-                            homephones[j - 1] = homephones[j];
-                            mobilephones[j] = temp;
-                            homephones[j] = temp2;
-                        }
-                    }
-                }
-                cout << "Вот отсортированный массив мобильных телефонов по возрастанию: \n";
-                for (long long phone : mobilephones) {
-                    cout << phone << ' ';
-                }
-                cout << "\nВот отсортированный массив домашних телефонов в соответствии с мобильными: \n";
-                for (long long phone : homephones) {
-                    cout << phone << ' ';
-                }
-                cout << '\n';
-                break;
-            }
-            case 2: {
-                for (int i = 0; i < size; i++) {
-                    for (int j = size - 1; j > i; j--) {
-                        if (homephones[j - 1] > homephones[j]) {
-                            long long temp = homephones[j - 1];
-                            long long temp2 = mobilephones[j - 1];
-                            mobilephones[j - 1] = mobilephones[j];
-                            homephones[j - 1] = homephones[j];
-                            homephones[j] = temp;
-                            mobilephones[j] = temp2;
-                        }
-                    }
-                }
-                cout << "Вот отсортированный массив мобильных телефонов в соответствии с домашними: \n";
-                for (long long phone : mobilephones) {
-                    cout << phone << ' ';
-                }
-                cout << "\nВот отсортированный массив домашних телефонов по возрастанию: \n";
-                for (long long phone : homephones) {
-                    cout << phone << ' ';
-                }
-                cout << '\n';
-                break;
-            }
-            }
-        }
-    }
+	/* {
+		cout << "\nFirst Task\n";
+		const int size = 5;
+		long long mobilephones[size] = { 3801234567,3705869854,3698745126,1522356587,1321254787 };
+		long long homephones[size] = { 9443654789,98745621,457456221,8765874512,9824512360 };
+		int choice;
+		cout << "Справочник\n";
+		while (true) {
+			choice = 0;
+			while (choice != 1 && choice != 2 && choice != 3 && choice != 4) {
+				cout << "Что вы хотите сделать? (1 - отсортировать по мобильным номерам,2 - отсортировать по домашним номерам,\n3 - вывести список пользователей, 4 - выход): ";
+				cin >> choice;
+			}
+			switch (choice) {
+			case 1: {
+				for (int i = 0; i < size; i++) {
+					for (int j = size - 1; j > i; j--) {
+						if (mobilephones[j - 1] > mobilephones[j]) {
+							long long temp = mobilephones[j - 1];
+							long long temp2 = homephones[j - 1];
+							mobilephones[j - 1] = mobilephones[j];
+							homephones[j - 1] = homephones[j];
+							mobilephones[j] = temp;
+							homephones[j] = temp2;
+						}
+					}
+				}
+				cout << "Вот отсортированный массив мобильных телефонов по возрастанию: \n";
+				for (long long phone : mobilephones) {
+					cout << phone << ' ';
+				}
+				cout << "\nВот отсортированный массив домашних телефонов в соответствии с мобильными: \n";
+				for (long long phone : homephones) {
+					cout << phone << ' ';
+				}
+				cout << '\n';
+				break;
+			}
+			case 2: {
+				for (int i = 0; i < size; i++) {
+					for (int j = size - 1; j > i; j--) {
+						if (homephones[j - 1] > homephones[j]) {
+							long long temp = homephones[j - 1];
+							long long temp2 = mobilephones[j - 1];
+							mobilephones[j - 1] = mobilephones[j];
+							homephones[j - 1] = homephones[j];
+							homephones[j] = temp;
+							mobilephones[j] = temp2;
+						}
+					}
+				}
+				cout << "Вот отсортированный массив мобильных телефонов в соответствии с домашними: \n";
+				for (long long phone : mobilephones) {
+					cout << phone << ' ';
+				}
+				cout << "\nВот отсортированный массив домашних телефонов по возрастанию: \n";
+				for (long long phone : homephones) {
+					cout << phone << ' ';
+				}
+				cout << '\n';
+				break;
+			}
+			case 3: {
+				cout << "Вот список пользователей (их телефоны):\n";
+				for (int i = 0; i < size; i++) {
+					cout << i + 1 << " пользователь по списку: ";
+					cout << "Мобильный номер: " << mobilephones[i] << "  Домашний номер: " << homephones[i] << '\n';
+				}
+				break;
+			}
+			}
+			if (choice == 4) {
+				cout << "До свидания\n"; break;
+			}
+		}
+	}*/
+
+	/* {
+		cout << "\nSecond Task\n";
+		const int size = 8;
+		int max = 500, min = 0, changes = 1;
+		int masiv[size] = {};
+		for (int i = 0; i < size; i++) {
+			masiv[i] = rand() % (max - min + 1) + min;
+		}
+		for (int i = 0; i < size; i++) {
+			changes = 0;
+			for (int j = size - 1; j > i; j--) {
+				if (masiv[j - 1] > masiv[j]) {
+					int temp = masiv[j - 1];
+					masiv[j - 1] = masiv[j];
+					masiv[j] = temp;
+					changes += 1;
+				}
+			}
+			if (changes == 0) {
+				break;
+			}
+		}
+		cout << "Масив отсортирован, вот он: \n";
+		for (int num : masiv) {
+			cout << num << ' ';
+		}
+	}*/
+
+	{
+		cout << "\nThird Task\n";
+		const int size = 10;
+		int oladushki[size] = { 4,67,76,12,345,6,7,89,90,23 };
+		int maxolad,maxoladindex;
+		int stop = 0, start = 0;
+		while (stop != 10) {
+			maxolad = oladushki[start];
+			maxoladindex = start;
+			for (int i = start; i < size; i++) {
+				if (maxolad < oladushki[i]) {
+					maxolad = oladushki[i];
+					maxoladindex = i;
+				}
+			}
+
+		}
+	}
 }
-
