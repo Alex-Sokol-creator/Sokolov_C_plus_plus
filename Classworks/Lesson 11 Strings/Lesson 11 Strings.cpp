f0048e26703bb94b04c95b9224fae1a280b7c5da
Alex-Sokol-creator <gamerodessa2019@gmail.com>
2026-06-06 14:50:52 +0300

    Lesson 11 was created and Dz 9 Second Task was almost done

---
+++ Classworks/Lesson 11 Strings/Lesson 11 Strings.cpp
@@ -0,0 +1,111 @@
+﻿
+#include <iostream>
+#include <Windows.h>;
+
+using namespace std;
+
+//Функция, которая получает рядок и возвращает количество символов в предложении
+int myStrlenth(const char* arr) {
+    int count = 0;
+    for (int i = 0; arr[i] != '\0'; i++) {
+        count++;
+    }
+    return count;
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    //string myStr = "hello, world!"; // ЗАПРЕЩЕНО ИСПОЛЬЗОВАТЬ!!!
+    //cout << myStr << '\n';
+
+    /*
+    char myStr[] = "hello, world!"; // string (предложения) в стиле С
+    cout << myStr << '\n';
+
+    char myStr2[5]; //TTTTT
+    for (int i = 0; i < 4; i++) {
+        myStr2[i] = 'T';
+    }
+    myStr2[4] = '\0'; // ноль-терминатор
+    cout << myStr2 << '\n';
+    */
+
+    /*
+
+    cout << (int)'H' << '\n';
+    cout << (int)'9' << '\n';
+    cout << (int)'#' << '\n';
+
+    cout << (int)'A' << ' ' << (int)'a' << '\n';
+    cout << ('A' > 'a') << '\n';
+
+    cout << (char)165 << '\n';
+    */
+
+    /*
+    char32_t emoji = U'😎';
+    cout << emoji << '\n';
+    */
+
+    /*
+
+    char myStr[] = "hello, world!";
+
+    char* ptr = myStr;
+    // myStr = "goodbye";
+
+    myStr[4] = 'u';
+    cout << myStr << '\n';
+
+    cout << "myStr length: " << myStrlenth("qwerty12345") << '\n';
+    cout << "myStr length: " << strlen("qwerty12345") << '\n';
+
+    char str1[] = "Hello, ", str2[] = "world!";
+    const int size = strlen(str1) + strlen(str2) + 1;
+    char* result = new char[strlen(str1) + strlen(str2) + 1]; // Hello, world!\0
+
+    strcpy_s(result, size, str1); // strcmp - копирует
+    strcat_s(result, size, str2); // strcat - ставит ноль-терминатор в конец
+    cout << result << '\n';
+    delete[] result;
+    */
+
+    char str1[] = "apple";
+    char str2[] = "Apple2";
+
+    int result = strcmp(str1, str2); // strcmp - сравнивает рядки
+
+    cout << result << '\n';
+
+    if (result >= 1) {
+        cout << "str1 is bigger than str2\n";
+    }
+    else if (result == 0) {
+        cout << "str1 is equal to str2\n";
+    }
+    else {
+        cout << "str1 is smaller than str2\n";
+    }
+
+    printf(str1);
+    printf("\nresult: %i", result);
+    printf("fraction: %.2f", 12.567788);
+
+    char fullName[255];
+    //cout << "\nEnter your name: ";
+    //cin >> fullName;
+    //cout << fullName << '\n';
+
+    scanf_s("%s", fullName, 255);
+    printf("Name: %s\n", fullName);
+
+    int age;
+    scanf_s("%d", &age);
+    printf("Age: %d\n", age);
+
+    fgets(fullName, 255, stdin);
+    cout << fullName << '\n';
+};
+
--- Homeworks/Dz 9 Динамические массивы/Dz 9 Динамические массивы.cpp
+++ Homeworks/Dz 9 Динамические массивы/Dz 9 Динамические массивы.cpp
@@ -6,128 +6,176 @@ using namespace std;
 
 /*
 int* AddGrade(int* grades, int& size, int newGrade) {
-    int* temp = new int[size + 1];
-    for (int i = 0; i < size;i++) {
-        temp[i] = grades[i];
-    }
-    temp[size] = newGrade;
-    size++;
-    delete[] grades;
-    grades = temp;
-    return grades;
+	int* temp = new int[size + 1];
+	for (int i = 0; i < size;i++) {
+		temp[i] = grades[i];
+	}
+	temp[size] = newGrade;
+	size++;
+	delete[] grades;
+	grades = temp;
+	return grades;
 }*/
 
 template <typename T>
 T* GiveMemory(int size) {
-    return new T[size];
+	return new T[size];
 }
 
 template <typename T>
 void Initialization(T* masiv, int size) {
-    for (int i = 0; i < size; i++) {
-        masiv[i] = rand();
-    }
+	for (int i = 0; i < size; i++) {
+		masiv[i] = rand();
+	}
 }
 
 template <typename T>
 void PrintMasiv(T* masiv, int size) {
-    cout << "Вот ваш динамический массив: \n";
-    for (int i = 0; i < size; i++) {
-        cout << masiv[i] << ' ';
-    }
+	cout << "Вот ваш динамический массив: \n";
+	for (int i = 0; i < size; i++) {
+		cout << masiv[i] << ' ';
+	}
 }
 
 template <typename T>
 void DeleteMemory(T* masiv) {
-    cout << "\nМассив удаляется из динамической памяти";
-    delete[] masiv;
+	cout << "\nМассив удаляется из динамической памяти";
+	delete[] masiv;
 }
 
 template <typename T>
-T* AddElement(T* masiv, int& size,T element) {
-    T* temp = new T[size + 1];
-    for (int i = 0; i < size; i++) {
-        temp[i] = masiv[i];
-    }
-    temp[size] = element;
-    size++;
-    delete[] masiv;
-    masiv = temp;
-    return masiv;
+T* AddElement(T* masiv, int& size, T element) {
+	T* temp = new T[size + 1];
+	for (int i = 0; i < size; i++) {
+		temp[i] = masiv[i];
+	}
+	temp[size] = element;
+	size++;
+	delete[] masiv;
+	masiv = temp;
+	return masiv;
 }
 
 template <typename T>
-T* AddElementByIndex(T* masiv, int& size,int index,T element) {
-    if (index < 0 || index > size) {
-        cout << "К сожалению, такого индекса нет в массиве\n";
-        return masiv;
-    }
-    T* temp = new T[size + 1];
-    for (int i = 0; i < index; i++) {
-        temp[i] = masiv[i];
-    }
-    temp[index] = element;
-    for (int i = index; i < size; i++) {
-        temp[i + 1] = masiv[i];
-    }
-    size++;
-    delete[] masiv;
-    masiv = temp;
-    return masiv;
+T* AddElementByIndex(T* masiv, int& size, int index, T element) {
+	if (index < 0 || index > size) {
+		cout << "К сожалению, такого индекса нет в массиве\n";
+		return masiv;
+	}
+	T* temp = new T[size + 1];
+	for (int i = 0; i < index; i++) {
+		temp[i] = masiv[i];
+	}
+	temp[index] = element;
+	for (int i = index; i < size; i++) {
+		temp[i + 1] = masiv[i];
+	}
+	size++;
+	delete[] masiv;
+	masiv = temp;
+	return masiv;
 }
 
 template <typename T>
-T* DeleteElementByIndex(T* masiv, int& size,int index) {
-    if (index < 0 || index >= size) {
-        cout << "К сожалению, такого индекса нет в массиве\n";
-        return masiv;
-    }
-    T* temp = new T[size - 1];
-    for (int i = 0; i < index; i++) {
-        temp[i] = masiv[i];
-    }
-    for (int i = index + 1; i < size; i++) {
-        temp[i - 1] = masiv[i];
-    }
-    size--;
-    delete[] masiv;
-    masiv = temp;
-    return masiv;
+T* DeleteElementByIndex(T* masiv, int& size, int index) {
+	if (index < 0 || index >= size) {
+		cout << "К сожалению, такого индекса нет в массиве\n";
+		return masiv;
+	}
+	T* temp = new T[size - 1];
+	for (int i = 0; i < index; i++) {
+		temp[i] = masiv[i];
+	}
+	for (int i = index + 1; i < size; i++) {
+		temp[i - 1] = masiv[i];
+	}
+	size--;
+	delete[] masiv;
+	masiv = temp;
+	return masiv;
+}
+
+template <typename T>
+T* DeleteSimpleNumbers(T* masiv, int& size) {
+	int countofsimples = 0;
+	int countofsimplessize = 0;
+	T* masivofsimplesindexes = new T[countofsimplessize];
+	for (int i = 0; i < size; i++) {
+		int countdividers = 0;
+		for (int j = 1; j <= masiv[i]; j++) {
+			if (masiv[i] % j == 0) {
+				countdividers++;
+			}
+		}
+		if (countdividers == 2) {
+			countofsimples++;
+			T* temp = new T[countofsimplessize + 1];
+			for (int c = 0; c < countofsimplessize; c++) {
+				temp[c] = masivofsimplesindexes[c];
+			}
+			temp[countofsimplessize] = i;
+			countofsimplessize++;
+			delete[] masivofsimplesindexes;
+			masivofsimplesindexes = temp;
+		}
+	}
+	int newsize = size - countofsimples;
+	int oldsize = size;
+	size = newsize;
+	T* newmasiv = new T[newsize];
+	int place = 0;
+	for (int i = 0; i < oldsize; i++) {
+		int checking = 0;
+		for (int c = 0; c < countofsimplessize;c++) {
+			if (i == masivofsimplesindexes[c]) {
+				checking = -1;
+				break;
+			}
+		}
+		if (checking != -1) {
+			newmasiv[place] = masiv[i];
+			place++;
+		}
+	}
+	return newmasiv;
 }
 
 int main()
 {
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
-    srand(time(NULL));
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	srand(time(NULL));
 
-    {
-        cout << "\nFirst Task\n";
-        int size = 6;
-        int num,index;
-        int* masiv = GiveMemory<int>(size);
-        Initialization(masiv, size);
-        PrintMasiv(masiv, size);
-        cout << "\nКакое число вы хотите добавить в конец массива?: ";
-        cin >> num;
-        masiv = AddElement(masiv, size, num);
-        PrintMasiv(masiv, size);
-        cout << "\nВ какой индекс вставить ваше число?: ";
-        cin >> index;
-        cout << "Напишите число, которое хотите вставить: ";
-        cin >> num;
-        masiv = AddElementByIndex(masiv, size, index, num);
-        PrintMasiv(masiv, size);
-        cout << "\nКакой индекс у элемента, который вы хотите удалить?: ";
-        cin >> index;
-        masiv = DeleteElementByIndex(masiv, size, index);
-        PrintMasiv(masiv,size);
-        DeleteMemory(masiv);
-    }
+	{
+		cout << "\nFirst Task\n";
+		int size = 6;
+		int num, index;
+		int* masiv = GiveMemory<int>(size);
+		Initialization(masiv, size);
+		PrintMasiv(masiv, size);
+		cout << "\nКакое число вы хотите добавить в конец массива?: ";
+		cin >> num;
+		masiv = AddElement(masiv, size, num);
+		PrintMasiv(masiv, size);
+		cout << "\nВ какой индекс вставить ваше число?: ";
+		cin >> index;
+		cout << "Напишите число, которое хотите вставить: ";
+		cin >> num;
+		masiv = AddElementByIndex(masiv, size, index, num);
+		PrintMasiv(masiv, size);
+		cout << "\nКакой индекс у элемента, который вы хотите удалить?: ";
+		cin >> index;
+		masiv = DeleteElementByIndex(masiv, size, index);
+		PrintMasiv(masiv, size);
+		DeleteMemory(masiv);
+	}
 
-    {
-        cout << "\nSecond Task\n";
-
-    }
+	{
+		cout << "\nSecond Task\n";
+		int size = 14;
+		int* masiv = new int[size];
+		int* result = DeleteSimpleNumbers(masiv, size);
+		PrintMasiv(result, size);
+	}
 }
 
