31c4886e93f26aeb2962e1345c5386681e991dd1
Alex-Sokol-creator <gamerodessa2019@gmail.com>
2026-06-07 14:46:22 +0300

    Lesson 11 was created and Dz 9 was done

---
+++ Classworks/Lesson 12 Кодирование символов и строки в формате языка С/Lesson 12 Кодирование символов и строки в формате языка С.cpp
@@ -0,0 +1,51 @@
+﻿
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+void PrintArr(int* arr, int size) {
+    cout << '\n';
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << ' ';
+    }
+    cout << '\n';
+}
+
+int* Initialization(int* arr, int size) {
+    for (int i = 0; i < size; i++) {
+        arr[i] = i * 10;
+    }
+    return arr;
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int* ptr = new int[5];
+    delete[] ptr;
+
+    int size = 10;
+    // void* 
+    int* arr = (int*)malloc(sizeof(int) * size);
+
+    free(arr);
+
+    int* arr2 = (int*)calloc(size, sizeof(int));
+    PrintArr(arr2, size);
+    Initialization(arr2, size);
+    PrintArr(arr2, size);
+    free(arr2);
+
+    int* arr3 = (int*)malloc(sizeof(int) * size);
+    Initialization(arr3, size);
+    size = 15;  
+    arr3 = (int*)realloc(arr3,size * sizeof(int));
+    PrintArr(arr3, size);
+    free(arr3);
+
+
+}
+
--- Homeworks/Dz 9 Динамические массивы/Dz 9 Динамические массивы.cpp
+++ Homeworks/Dz 9 Динамические массивы/Dz 9 Динамические массивы.cpp
@@ -1,6 +1,6 @@
 ﻿
 #include <iostream>
-#include <Windows.h>;
+#include <Windows.h>
 
 using namespace std;
 
@@ -35,11 +35,12 @@ void PrintMasiv(T* masiv, int size) {
 	for (int i = 0; i < size; i++) {
 		cout << masiv[i] << ' ';
 	}
+	cout << '\n';
 }
 
 template <typename T>
 void DeleteMemory(T* masiv) {
-	cout << "\nМассив удаляется из динамической памяти";
+	cout << "\nМассив с адресом " << masiv << " удаляется из динамической памяти";
 	delete[] masiv;
 }
 
@@ -137,12 +138,132 @@ T* DeleteSimpleNumbers(T* masiv, int& size) {
 			place++;
 		}
 	}
+	delete[] masivofsimplesindexes;
+	delete[] masiv;
 	return newmasiv;
 }
 
 template <typename T>
 void SortMasiv(T* masiv, int size) {
-
+	int countofpluses = 0, countofnegatives = 0, countofzeros = 0;
+	int countofplusessize = 0, countofnegativessize = 0, countofzerossize = 0;
+	T* masivofplusesindexes = new T[countofplusessize];
+	T* masivofnegativesindexes = new T[countofnegativessize];
+	T* masivofzerosindexes = new T[countofzerossize];
+	for (int i = 0; i < size; i++) {
+		if (masiv[i] > 0) {
+			countofpluses++;
+			T* temp = new T[countofplusessize + 1];
+			for (int c = 0; c < countofplusessize; c++) {
+				temp[c] = masivofplusesindexes[c];
+			}
+			temp[countofplusessize] = i;
+			countofplusessize++;
+			delete[] masivofplusesindexes;
+			masivofplusesindexes = temp;
+		}
+		else if (masiv[i] < 0) {
+			countofnegatives++;
+			T* temp = new T[countofnegativessize + 1];
+			for (int c = 0; c < countofnegativessize; c++) {
+				temp[c] = masivofnegativesindexes[c];
+			}
+			temp[countofnegativessize] = i;
+			countofnegativessize++;
+			delete[] masivofnegativesindexes;
+			masivofnegativesindexes = temp;
+		}
+		else {
+			countofzeros++;
+			T* temp = new T[countofzerossize + 1];
+			for (int c = 0; c < countofzerossize; c++) {
+				temp[c] = masivofzerosindexes[c];
+			}
+			temp[countofzerossize] = i;
+			countofzerossize++;
+			delete[] masivofzerosindexes;
+			masivofzerosindexes = temp;
+		}
+	}
+	int newsizeofpluses = size - (countofnegatives + countofzeros);
+	int newsizeofnegatives = size - (countofpluses + countofzeros);
+	int newsizeofzeros = size - (countofnegatives + countofpluses);
+	T* newmasivofpluses = new T[newsizeofpluses];
+	T* newmasivofnegatives = new T[newsizeofnegatives];
+	T* newmasivofzeros = new T[newsizeofzeros];
+	int placeofpluses = 0, placeofnegatives = 0, placeofzeros = 0;
+	for (int i = 0; i < size; i++) {
+		int checking = 0;
+		for (int c = 0; c < countofnegativessize;c++) {
+			if (i == masivofnegativesindexes[c]) {
+				checking = -1;
+				break;
+			}
+		}
+		if (checking != -1) {
+			for (int c = 0; c < countofzerossize;c++) {
+				if (i == masivofzerosindexes[c]) {
+					checking = -1;
+					break;
+				}
+			}
+			if (checking != -1) {
+				newmasivofpluses[placeofpluses] = masiv[i];
+				placeofpluses++;
+			}
+		}
+	}
+	for (int i = 0; i < size; i++) {
+		int checking = 0;
+		for (int c = 0; c < countofplusessize;c++) {
+			if (i == masivofplusesindexes[c]) {
+				checking = -1;
+				break;
+			}
+		}
+		if (checking != -1) {
+			for (int c = 0; c < countofzerossize;c++) {
+				if (i == masivofzerosindexes[c]) {
+					checking = -1;
+					break;
+				}
+			}
+			if (checking != -1) {
+				newmasivofnegatives[placeofnegatives] = masiv[i];
+				placeofnegatives++;
+			}
+		}
+	}
+	for (int i = 0; i < size; i++) {
+		int checking = 0;
+		for (int c = 0; c < countofnegativessize;c++) {
+			if (i == masivofnegativesindexes[c]) {
+				checking = -1;
+				break;
+			}
+		}
+		if (checking != -1) {
+			for (int c = 0; c < countofplusessize;c++) {
+				if (i == masivofplusesindexes[c]) {
+					checking = -1;
+					break;
+				}
+			}
+			if (checking != -1) {
+				newmasivofzeros[placeofzeros] = masiv[i];
+				placeofzeros++;
+			}
+		}
+	}
+	PrintMasiv(newmasivofpluses, newsizeofpluses);
+	PrintMasiv(newmasivofnegatives, newsizeofnegatives);
+	PrintMasiv(newmasivofzeros, newsizeofzeros);
+	DeleteMemory(newmasivofpluses);
+	DeleteMemory(newmasivofnegatives);
+	DeleteMemory(newmasivofzeros);
+	delete[] masivofplusesindexes;
+	delete[] masivofnegativesindexes;
+	delete[] masivofzerosindexes;
 }
 
 int main()
@@ -158,17 +279,17 @@ int main()
 		int* masiv = GiveMemory<int>(size);
 		Initialization(masiv, size);
 		PrintMasiv(masiv, size);
-		cout << "\nКакое число вы хотите добавить в конец массива?: ";
+		cout << "Какое число вы хотите добавить в конец массива?: ";
 		cin >> num;
 		masiv = AddElement(masiv, size, num);
 		PrintMasiv(masiv, size);
-		cout << "\nВ какой индекс вставить ваше число?: ";
+		cout << "В какой индекс вставить ваше число?: ";
 		cin >> index;
 		cout << "Напишите число, которое хотите вставить: ";
 		cin >> num;
 		masiv = AddElementByIndex(masiv, size, index, num);
 		PrintMasiv(masiv, size);
-		cout << "\nКакой индекс у элемента, который вы хотите удалить?: ";
+		cout << "Какой индекс у элемента, который вы хотите удалить?: ";
 		cin >> index;
 		masiv = DeleteElementByIndex(masiv, size, index);
 		PrintMasiv(masiv, size);
@@ -182,15 +303,19 @@ int main()
 		Initialization(masiv, size);
 		int* result = DeleteSimpleNumbers(masiv, size);
 		PrintMasiv(result, size);
+		DeleteMemory(result);
 	}
 
 	{
 		cout << "\nThird Task\n";
-		const int size = 6;
-		int masiv[size];
-		Initialization(masiv, size);
-		int* ptr = &masiv;
-
+		const int size = 14;
+		int masiv[size] = {10,5,-12,-67,0,42,0,12,-76,-1990,0,14,1487,1234};
+		cout << "Вот изначальный массив: \n";
+		for (int i = 0; i < size; i++) {
+			cout << masiv[i] << ' ';
+		}
+		cout << "\nВот 3 отсортированных массива: \n";
+		SortMasiv(masiv, size);
 	}
 }
 
