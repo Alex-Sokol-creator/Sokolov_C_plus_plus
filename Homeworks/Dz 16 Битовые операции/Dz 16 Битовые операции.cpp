28bbbf5c4010fc2c41a93ee71efa7336a1efd6c9
Alex-Sokol-creator <gamerodessa2019@gmail.com>
2026-07-26 14:49:15 +0300

    Dz 16 was done and Practice 13 was too

---
+++ Homeworks/Dz 16 Битовые операции/Dz 16 Битовые операции.cpp
@@ -0,0 +1,98 @@
+﻿
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+char* NumToChar(int number) {
+    char* result = new char[33];
+    int index = 0;
+    for (int i = 31; i >= 0; i--) {
+        result[index] = ((number >> i) & 1) + '0';
+        index++;
+    }
+    result[index] = '\0';
+    return result;
+}
+
+char* NumToCharSixteen(int num) {
+    char* result = new char[9];
+    int index = 7;
+    for (int i = 0; i < 8; i++) {
+        int number_ready = num & 0xf;
+        if (number_ready < 10) {
+            result[index] = number_ready + '0';
+        }
+        else {
+            result[index] = (number_ready - 10) + 'A';
+        }
+        num = num >> 4;
+        index--;
+    }
+    result[8] = '\0';
+    return result;
+}
+
+int CharToNum(char* number) {
+    int size = strlen(number);
+    int result = 0, degree = 0, twokoeficient = 1;
+    for (int i = size - 1; i >= 0; i--) {
+        int multiply = (number[i] - '0') * twokoeficient;
+        result += multiply;
+        twokoeficient *= 2;
+    }
+    return result;
+}
+
+int CharToNumSixteen(char* number) {
+    int size = strlen(number);
+    int result = 0, degree = 0, sixteenkoeficient = 1;
+    for (int i = size - 1; i >= 0; i--) {
+        int symbol;
+        if (number[i] >= '0' && number[i] <= '9') {
+            symbol = number[i] - '0';
+        }
+        else {
+            symbol = (number[i] - 'A') + 10;
+        }
+        int multiply = symbol * sixteenkoeficient;
+        result += multiply;
+        sixteenkoeficient *= 16;
+    }
+    return result;
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    cout << "\nFirst Task\n";
+    int number;
+    cout << "Введите целое число, чтобы перевести его в двоичный формат: ";
+    cin >> number;
+    char* result = NumToChar(number);
+    cout << "После конвертации ваше число будет таким: " << result << '\n';
+    delete[] result;
+
+    cout << "\nSecond Task\n";
+    cout << "Введите целое число, чтобы перевести его в 16-й формат: ";
+    cin >> number;
+    result = NumToCharSixteen(number);
+    cout << "После конвертации ваше число будет таким: " << result << '\n';
+    delete[] result;
+
+    cout << "\nThird Task\n";
+    char number_str[40];
+    cin.ignore();
+    cout << "Введите число в двоичном формате, чтобы перевести его в десятичный формат: ";
+    cin.getline(number_str, 40);
+    int result_num = CharToNum(number_str);
+    cout << "После конвертации ваше число будет таким: " << result_num << '\n';
+
+    cout << "\nFourth Task\n";
+    cout << "Введите число в 16-м формате, чтобы перевести его в десятичный формат: ";
+    cin.getline(number_str, 40);
+    result_num = CharToNumSixteen(number_str);
+    cout << "После конвертации ваше число будет таким: " << result_num << '\n';
+}
---
+++ Practices/Practice 13 Указатель на функцию/Practice 13 Указатель на функцию.cpp
@@ -0,0 +1,78 @@
+﻿
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+template <typename T>
+T sum(T num1, T num2) {
+    return num1 + num2;
+}
+
+template <typename T>
+T subtruct(T num1, T num2) {
+    return num1 - num2;
+}
+
+template <typename T>
+T multiply(T num1, T num2) {
+    return num1 * num2;
+}
+
+
+template <typename T>
+T divide(T num1, T num2) {
+    return num1 / num2;
+}
+
+template <typename T>
+void calcAndPrint(T num1, T num2, T (*callback)(T, T)) {
+    cout << callback(num1, num2) << '\n';
+}
+
+template <typename T>
+void UnionOfMasivs(T* A, T* B, T* C, int sizeABC) {
+    for (int i = 0; i < sizeABC; i++) {
+        C[i] = A[i] + B[i];
+    }
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    srand(time(NULL));
+
+    cout << "\nFirst Task\n";
+    double (*calculation[48]) (double, double) = { nullptr };
+    calculation['+'] = { sum };
+    calculation['-'] = { subtruct };
+    calculation['*'] = { multiply };
+    calculation['/'] = { divide };
+
+    double num1, num2;
+    char operation;
+    cout << "Введите 1 число: ";
+    cin >> num1;
+    cout << "Введите 2 число: ";
+    cin >> num2;
+    cout << "Введите операцию, которую хотите произвести с данными числами (+,-,*,/): ";
+    cin >> operation;
+    cout << "После расчетов, ваш результат будет: ";
+    calcAndPrint(num1, num2, calculation[operation]);
+    
+    cout << "\nSecond Task\n";
+    const int sizeABC = 10;
+    int min = -100, max = 100;
+    int A[sizeABC], B[sizeABC], C[sizeABC];
+    for (int i = 0; i < sizeABC; i++) {
+        A[i] = min + rand() % (max - min + 1);
+        B[i] = min + rand() % (max - min + 1);
+    }
+    UnionOfMasivs(A, B, C, sizeABC);
+    cout << "Суммы попарных элементов в массивах А и Б были занесены в массив С, вот они: \n";
+    for (int i = 0; i < sizeABC; i++) {
+        cout << "Сумма " << i + 1 << " элементов: " << C[i] << '\n';
+    }
+}
+
